e373bac8c922b285e82205ff79374610e1eefda7
Khushbu <khushbu.priyadarshni3@gmail.com>
2022-08-15 14:21:45 +0530

    My Django Girls app, first commit

---
+++ python.py
@@ -0,0 +1,14 @@
+def myfirstprg(name):
+    print('this function is written by '+name+" !")
+
+girls = ['Khushbu','Khushi','Gudi']
+
+for i in girls:
+    myfirstprg(i)
+
+if 2>3:
+    print('Hello Khushbu! you are doing so good. Kudo\'s to you')
+else:
+    print('The comparison returned False')
+
+
